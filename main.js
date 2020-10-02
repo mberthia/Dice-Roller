c839e671a02008c1cf0db39decf80d94f0b5998f
mberthia <mberthiaume00@gmail.com>
2020-10-02 16:21:35 -0400

    basic objectives done

--- main.js
+++ main.js
@@ -9,7 +9,24 @@ rollButton.addEventListener("click", function () {
   let value = Number(userInput.value);
   let index = 0;
   while (value > index) {
-    console.log(Math.floor(Math.random() * 6 + 1));
+    dieResult = Math.floor(Math.random() * 6 + 1);
+    dieRolls.push(dieResult);
+    console.log(dieRolls);
+    diceSum.innerHTML = dieRolls.reduce(addArray);
+    function addArray(total, Number) {
+      return total + Number;
+    }
     index++;
+    console.log(dieRolls.length);
+  }
+});
+
+showButton.addEventListener("click", function () {
+  let index1 = 0;
+  while (dieRolls.length > index1) {
+    let fullList = document.createElement("li");
+    fullList.innerHTML = dieRolls[index1];
+    document.getElementById("show-rolls").appendChild(fullList);
+    index1++;
   }
 });
